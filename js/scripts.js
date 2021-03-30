f82549b90c330c8a6fa600b81e0642a6bd9462be
Mateusz Wyrębek <mateuszw774@gmail.com>
2021-03-30 22:13:23 +0200

    Photo website

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
+
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+  // options
+
+  wrapAround: true,
+  fade: true,
+  
+});
+
